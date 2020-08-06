fa705b609393cffd7dfbaa21ecc60cc86e74c3ab
Denis Naughton <mjolinor75@hotmail.com>
2020-08-06 14:05:38 -0400

    Added test for home, ap seminar and intro

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,34 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "South Shore Studies"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get intro" do
     get static_pages_intro_url
     assert_response :success
+    assert_select "title", "Intro | #{@base_title}"
   end
 
   test "should get ap_seminar" do
   	get static_pages_ap_seminar_url
   	assert_response :success
+    assert_select "title", "AP Seminar | #{@base_title}"
   end
 
 end
